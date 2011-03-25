bc33844aa83d91be43c08a78160298a4686fde0e
Jeff Jankowski <jeff@visualo.us>
2011-03-24 23:04:52 -0400

    Initial commit. Scaffolding demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a0eb9d07e9bc2505a399039637e1f6383f4000029de49d55334f5517f6c8d5c9f5c9fd92d7a9b983b44c2f38e0009c909e2549605aa550d051ba2676eba0bd74'
